89bf71dbb73775fdbc4e797ddc31ccbc7c3ab53e
AstorasDov <astorasdov@gmail.com>
2025-06-25 15:28:46 +0300

    Update app.js

--- client/app.js
+++ client/app.js
@@ -1,70 +1,58 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-canvas.width = 300;
-canvas.height = 300;
-
-const unitSize = 2;
-
 class Vector3 {
     constructor(x=0,y=0,z=0){
         this.x = x;
         this.y = y;
         this.z = z;
-    };scaled(scalingFactor){
+    };scaled(s){
         const {x,y,z} = this;
-        const s = scalingFactor;
         return new Vector3(x*s,y*s,z*s);
-    };translated(x,y,z){
-        return new Vector3(
-            this.x + x,
-            this.y + y,
-            this.z + z,
-        );
-    };renderOnCamera(camera){
-        const translatedVector = this.translated(-camera.position.x,-camera.position.y,-camera.position.z);
-        const scalingFactor = camera.focalLength / translatedVector.z;
-        const scaledVector = translatedVector.scaled(scalingFactor);
-        ctx.fillStyle = "black";
-        ctx.fillRect(
-            canvas.width/2+scaledVector.x,
-            canvas.height/2-scaledVector.y,
-            unitSize,unitSize
-        );
+    };translated(i,j,k){
+        const {x,y,z} = this;
+        return new Vector3(x+i,y+j,z+k);
+    };renderOnCanvas(camera,context){
+        const translatedVector = this.translated(-camera.pos.x,-camera.pos.y,-camera.pos.z);
+        const s = camera.focal / translatedVector.z;
+        const scaled = translatedVector.scaled(s);
+        context.fillStyle = "black";
+        context.fillRect(canvas.width/2+scaled.x,canvas.height/2-scaled.y,5,5);
+    }
+}
+
+class Triangle {
+    constructor(a=new Vector3,b=new Vector3,c=new Vector3){
+        this.a=a;this.b=b;this.c=c;
     }
 }
 
 class Camera {
-    constructor(position=new Vector3(),focalLength=1000){
-        this.position = position;
-        this.focalLength = focalLength;
+    constructor(pos=new Vector3,focal=500){
+        this.pos = pos;
+        this.focal = focal;
     }
 }
 
-const userCamera = new Camera(new Vector3(0,0,-20),1000);
+const userCamera = new Camera(new Vector3(0,0,-2),500);
 const vertices = [];
+const triangles = [];
 
-const render = () => {
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    vertices.forEach(v=>v.renderOnCamera(userCamera));
-}
+window.addEventListener("DOMContentLoaded",async()=>{
+    const response = await fetch('/api/model');
+    const object = await response.json();
 
+    const {v,f} = object;
 
-window.addEventListener("DOMContentLoaded",async ()=>{
-    const response = await fetch('/api/loadModel');
-    const vertexData = await response.json();
-    for(const vertice of vertexData){
-        vertices.push(new Vector3(vertice[0],vertice[1],vertice[2]));
-    };render();
-});
-
-const step = 1;
-window.addEventListener("keydown",(e)=>{
-    const movementKeys = ['a','w','s','d'];
-    if(e.key==="w"){userCamera.position.z+=step}
-    else if(e.key==="s"){userCamera.position.z-=step}
-    else if(e.key==="a"){userCamera.position.x-=step}
-    else if(e.key==="d"){userCamera.position.x+=step}
-
-    if(movementKeys.includes(e.key)){render();}
+    for(const vertex of v){
+        vertices.push(new Vector3(vertex[0],vertex[1],vertex[2]));
+    };for(const triangle of f){
+        triangles.push(
+            new Triangle(
+                vertices[triangle[0]-1],
+                vertices[triangle[1]-1],
+                vertices[triangle[2]-1]
+            )
+        );
+    };console.log(triangles);
 });
