8a313694aae0f06fe6d2f80c3b278ad3703a8473
AstorasDov <astorasdov@gmail.com>
2025-07-02 15:32:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const {readFile} = require('fs');
 const path = require('path');
+
 const read = (path) => {
     return new Promise((resolve,reject)=>{
         readFile(path,'utf-8',(err,data)=>{
@@ -10,39 +11,30 @@ const read = (path) => {
     });
 }
 
-const server = http.createServer(async(req,res)=>{
+const server = http.createServer(async (req,res)=>{
     const extname = path.extname(req.url);
+
     switch(req.url){
         case "/":
-            const html = await read('./client/index.html');
+            const html = await read('./public/index.html');
             res.writeHead(200,{"Content-Type":"text/html"});
-            res.end(html);
-            break;
-        case "/api/model":
-            const data = await read('./Model/model.obj');
-            const lines = data.split('\n');
-
-            const vertexData = lines.filter(l=>l.startsWith('v '))
-            .map(l=>l.replace('v ',"").trim().split(' ').map(n=>parseFloat(n)));
-
-            const faceData = lines.filter(f=>f.startsWith('f '))
-            .map(f=>f.replace('f ',"").trim().split(' ').map(n=>parseFloat(n)));
-
-            res.writeHead(200,{"Content-Type":"application/json"});
-            res.end(JSON.stringify({v:vertexData,f:faceData}));
+            res.write(html);
+            res.end();
             break;
     }
 
     switch(extname){
         case ".css":
-            const css = await read(path.join(__dirname,'client',req.url));
-            res.writeHead(200,{"Content-type":"text/css"})
-            res.end(css);
+            const css = await read(path.join(__dirname,'public',req.url));
+            res.writeHead(200,{"Content-Type":"text/css"});;
+            res.write(css);
+            res.end();
             break;
         case ".js":
-            const js = await read(path.join(__dirname,'client',req.url));
-            res.writeHead(200, {"Content-Type": "application/javascript"});
-            res.end(js);
+            const js = await read(path.join(__dirname,'public',req.url));
+            res.writeHead(200,{"Content-Type":"application/javascript"});;
+            res.write(js);
+            res.end();
             break;
     }
 });
